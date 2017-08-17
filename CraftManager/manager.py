813625734534312a51a46edcde3e9679d385f7c2
Ytturi <jaume.florez93@gmail.com>
2017-08-17 20:41:36 +0200

    Add task processing and update utils to manager

--- CraftManager/manager.py
+++ CraftManager/manager.py
@@ -1,6 +1,11 @@
 # -*- encoding utf-8 -*-
 import logging
 
+_RES_OK = 0
+_RES_WARN = 1
+_RES_ERR = 2
+_RES_CRIT = 3
+_RES_EXIT = 51 # ExIT -> 3x17 => 51
 
 _TASK_HELP = ['h', 'help', 'options', 'tasks']
 _TASK_INFO = ['i', 'info']
@@ -54,56 +59,58 @@ COLORS = {
 }
 
 
-class CraftUtils:
+def color_string(colour, text):
+    if colour in _AVAILABLE_COLORS:
+        return '{}{}{}'.format(
+            COLORS[colour], text, COLORS['reset']
+        )
+    else:
+        return text
+
+
+class CraftManager:
     def __init__(self, logger):
         self.logger = logger
 
+    # Log utilities
     def msg(self, text):
         text = '> {text}'.format(**locals())
         self.logger.info(text)
 
     def info(self, text):
-        colored_info = self.color_string('green', 'INFO')
+        colored_info = color_string('green', 'INFO')
         text = '{colored_info}: {text}'.format(**locals())
         self.logger.info(text)
 
     def debug(self, text):
-        colored_info = self.color_string(
+        colored_info = color_string(
             'blue', 'DEBUG: {text}'.format(**locals())
         )
         text = '{colored_info}'.format(**locals())
         self.logger.debug(text)
 
     def warn(self, text):
-        colored_info = self.color_string('yellow', 'WARNING')
+        colored_info = color_string('yellow', 'WARNING')
         text = '{colored_info}: {text}'.format(**locals())
         self.logger.warning(text)
 
     def error(self, text):
-        colored_info = self.color_string('red', 'ERROR')
+        colored_info = color_string('red', 'ERROR')
         text = '{colored_info}: {text}'.format(**locals())
         self.logger.error(text)
 
     def critical(self, text):
-        colored_info = self.color_string(
+        colored_info = color_string(
             'red', 'CRITICAL: {text}'.format(**locals())
         )
         text = '{colored_info}'.format(**locals())
         self.logger.debug(text)
 
-    @staticmethod
-    def color_string(colour, text):
-        if colour in _AVAILABLE_COLORS:
-            return '{}{}{}'.format(
-                COLORS[colour], text, COLORS['reset']
-            )
-        else:
-            return text
-
+    # Prompts and messages
     def print_menu(self):
         msg = 'Available Tasks:\n'
         for words, description, args in _TASKS:
-            tag = self.color_string('yellow', words[1])
+            tag = color_string('yellow', words[1])
             msg += '>[{0}]{1}\n\t{0} {2}\n'.format(tag, description, args)
         self.info(msg)
     
@@ -113,3 +120,38 @@ class CraftUtils:
             msg += '\t{}\t{}\n'.format(action, description)
         self.info(msg)
 
+    # Task processing
+    def valid_taskname(self, taskname):
+        for tags, descr, args in _TASKS:
+            if taskname.lower() in tags:
+                return True
+        return False
+
+    def run_task(self, task_name):
+        if not task_name:
+            self.error('No task name or tag provided')
+            return self.task_error
+        elif not self.valid_taskname(task_name):
+            self.error('Task "{}" does not exist'.format(task_name))
+            return self.task_error
+        self.debug('Running task "{}"'.format(task_name))
+        if task_name == _TASK_HELP:
+            return self.print_menu
+        elif task_name == _TASK_EXIT:
+            return self.task_exit
+        else:
+            return self.task_not_implemented
+     
+    def task_error(self, taskname, args):
+        self.debug(
+            'Default error for TASK: [{taskname}|{args}]'.format(**locals())
+        )
+        return _RES_ERR
+
+    def task_not_implemented(self, taskname, args):
+        self.warn('Task not implemented yet!')
+        return _RES_WARN
+
+    def task_exit(self, taskname, args):
+        self.debug('Exiting manager')
+        return _RES_EXIT
