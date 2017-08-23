10a88adbff27da3968f59b9a6d3c12080b830420
Ytturi <jaume.florez93@gmail.com>
2017-08-23 23:37:05 +0200

    Add _MODEL_RECIPE test

--- spec/manager_spec.py
+++ spec/manager_spec.py
@@ -11,11 +11,41 @@ with description('CraftManager Class'):
                 'stock': 0,
                 'recipe_id': 1,
             }
-            man_product = manager._MODEL_PRODUCT(
+            product_obj = manager._MODEL_PRODUCT(
                 self_id=pars['self_id'], name=pars['name'],
                 stock=pars['stock'], recipe_id=pars['recipe_id']
             )
-            product_dict = man_product._asdict()
+            product_dict = product_obj._asdict()
             expect(len(product_dict.keys())).to(equal(len(pars.keys())))
             for key, value in product_dict.items():
                 expect(value).to(equal(pars[key]))
+
+        with it('must have a _MODEL_RECIPE as specified in the docs'):
+            pars = {
+                'self_id': 1,
+                'result_id': 1,
+                'requirement_id_1': 1,
+                'requirement_amount_1': 1,
+                'requirement_id_2': 2,
+                'requirement_amount_2': 1,
+                'requirement_id_3': 3,
+                'requirement_amount_3': 1,
+                'requirement_id_4': False,
+                'requirement_amount_4': False,
+            }
+            recipe_obj = manager._MODEL_RECIPE(
+                self_id=pars['self_id'], result_id=pars['result_id'],
+                requirement_id_1=pars['requirement_id_1'],
+                requirement_amount_1=pars['requirement_amount_1'],
+                requirement_id_2=pars['requirement_id_2'],
+                requirement_amount_2=pars['requirement_amount_2'],
+                requirement_id_3=pars['requirement_id_3'],
+                requirement_amount_3=pars['requirement_amount_3'],
+                requirement_id_4=pars['requirement_id_4'],
+                requirement_amount_4=pars['requirement_amount_4'],
+            )
+            recipe_dict = recipe_obj._asdict()
+            expect(len(recipe_dict.keys())).to(equal(len(pars.keys())))
+            for key, value in recipe_dict.items():
+                expect(value).to(equal(pars[key]))
+
